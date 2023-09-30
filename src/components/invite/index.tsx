3db87caa7587b4635027163c38e98c89518e1b87
Ivan <duremar98@yandex.ru>
2023-09-30 20:13:35 +0300

    sendEmail

--- src/components/invite/index.tsx
+++ src/components/invite/index.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as SvgArrow } from "../../assets/arrowLit.svg";
 import { Content, Form } from "./style";
 
 const Invite = () => {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
+    const data = {
+      Name: name,
+      Phone: phone,
+      Email: email,
+    };
+    console.log("data", data);
     e.currentTarget.reset();
+    setName("");
+    setPhone("");
+    setEmail("");
   };
 
   return (
@@ -16,11 +28,29 @@ const Invite = () => {
           Приглашаем к сотрудничеству производителей и поставщиков одежды, обуви
           и аксессуаров
         </h4>
-        <input type="text" placeholder="Ваше имя" name="user_name" />
-        <input type="tel" placeholder="Номер телефона" name="message" />
-        <input type="email" placeholder="Электронная почта" name="user_email" />
+        <input
+          required
+          type="text"
+          placeholder="Ваше имя"
+          name="user_name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          required={email ? false : true}
+          type="tel"
+          placeholder="Номер телефона"
+          name="message"
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <input
+          required={phone ? false : true}
+          type="email"
+          placeholder="Электронная почта"
+          name="user_email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <div>
-          <input type="checkbox" />{" "}
+          <input required type="checkbox" />{" "}
           <span>Даю согласие на обработку персональных данных</span>
         </div>
         <button type="submit">
